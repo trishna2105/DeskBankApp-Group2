a8f8b2452fe733e141df0569d7232f32eddf5942
Trishna <trishna2104@gmail.com>
2026-08-19 13:28:04 +1000

    Completed Python Bank solution

--- bank.py
+++ bank.py
@@ -1,11 +1,52 @@
 class Bank:
-    @staticmethod
+    def __init__(self):
+        self.balance=1000.0
+
+    def deposit(self,amount):
+        self.balance+=amount 
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            raise ValueError("Insufficient funds")
+        self.balance-=amount 
+
+    def show_balance(self):
+        print(f"Starting balance: ${self.balance:.2f}")
+
+    #while True:
+       # choice=input("Continue banking (d/w/s/x): ").lower()
+
+
     def main():
+        bank=Bank()
+        choice=input("Start banking :(d/w/s/x): ").lower()
+        while choice!="x":
+            if choice=="d":
+                amount=float(input("Amount to deposit:$"))
+                bank.deposit(amount)
+                print(f"Amount deposited:${amount:.2f}")
+            elif choice=="w":
+                amount=float(input("Amount to withdraw:$"))
+                try:
+                    bank.withdraw(amount)
+                    print(f"Amount withdrawn: ${amount:.2f}")
+                except ValueError as error:
+                    print(error)
+            elif choice=="s":
+                bank.show_balance()
+            else:
+                print("Invalid choice")
+            choice=input("Continue banking (d/w/s/x): ").lower()
+
+
+
+"""
         print("DeskBankApp started")
         deposit_amount=float(input("Amount to be desposited in $"))
         print(f"Amount ${deposit_amount:.2f} deposited")
         withdrawn_amount=float(input("Amount to be withdrawn in $"))
         print(f"Amount ${withdrawn_amount:.2f} withdrawn")
+"""
 
 if __name__=="__main__":
     Bank.main()
